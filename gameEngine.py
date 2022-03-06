01dd02f0d5589a40ba832ed96ef6bdcf6fe72a94
Aleksandar <aleksandar_lukic@outlook.com>
2022-03-06 19:56:05 +0100

    Created a simple layout for the board to be printed in the terminal, displaying both player sides with the pits in between

--- gameEngine.py
+++ gameEngine.py
@@ -3,7 +3,8 @@
 playing = True
 
 # Pits to hold stones and the two players end pits
-pits = [4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0]
+pits = [0, 4, 4, 4, 4, 4, 4, # Player 1 in index 0 to 6 
+        4, 4, 4, 4, 4, 4, 0] # Player 2 in index 7 to 13
     
 
 # Game loop
@@ -12,13 +13,35 @@ while(playing):
     # loop to format number in pits
     i = 0
     for pit in pits:
+        pits[i] = int(pits[i])
         if int(pits[i]) < 10:
             pits[i] = " " + str(pits[i])
         else:
             pits[i] = str(pits[i])
         i = i + 1
-      
-    print(pits)
+    
+    # Print top border of board
+    print("o----------------------------------o")
+        
+    # Print Player 1:  
+    playerOneSide = "|    | "
+    for i in range(1, 6):
+        playerOneSide = playerOneSide + str(pits[i]) + " | "
+    playerOneSide = playerOneSide + "   |"
+    print(playerOneSide)
+    
+    # Print player pits and seperation between sides
+    print("| " + pits[0] + " |----|----|----|----|----| " + pits[13] + " |")
+    
+    # Print Player 2:  
+    playerTwoSide = "|    | "
+    for i in range(7, 12):
+        playerTwoSide = playerTwoSide + str(pits[i]) + " | "
+    playerTwoSide = playerTwoSide + "   |"
+    print(playerTwoSide)
+    
+    # Print bottom border of board
+    print("o----------------------------------o")
     
     userInput = input()
     
