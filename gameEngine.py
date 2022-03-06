e774307a4e34d64e9d9ffdcd64f0765e37d5b426
Aleksandar <aleksandar_lukic@outlook.com>
2022-03-06 18:56:30 +0100

    Created a for loop to format in an extra space in the case of numbers <10

--- gameEngine.py
+++ gameEngine.py
@@ -8,6 +8,15 @@ pits = [4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0]
 
 # Game loop
 while(playing):
+    
+    # loop to format number in pits
+    i = 0
+    for pit in pits:
+        if int(pits[i]) < 10:
+            pits[i] = " " + str(pits[i])
+        else:
+            pits[i] = str(pits[i])
+        i = i + 1
       
     print(pits)
     
