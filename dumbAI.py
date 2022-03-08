0d62079ecc198c56b52a5054d951b2c61e57180b
Hjalte <hjaltepind@gmail.com>
2022-03-08 11:54:54 +0100

    Added comments and considering going round the board

--- dumbAI.py
+++ dumbAI.py
@@ -1,7 +1,10 @@
 def nextMove(pits):
+    # Simple, greedy AI, that chooses next move from pits, assuming to be player 2. The move is chosen to be the
+    # first pile closest to the home pile that ends in the home pile, or if none does, the closest pile to the home
+    # pile with the most marbles
     maxPit = 5
     for i in range(5, -1, -1):
-        if pits[i] == 6 - i:
+        if (pits[i] % 14) == 6 - i:
             return i
         elif pits[i] > pits[maxPit]:
             maxPit = i
